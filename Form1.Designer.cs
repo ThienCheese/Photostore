d7daf9a5eac3cb7ad9f9d2ea30d4f54c1ee47070
Chí Nguyễn Thiện <ngthienchi05@gmail.com>
2024-09-16 15:13:22 +0700

    finish

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,6 +44,7 @@
             // 
             // flowLayoutPanel1
             // 
+            flowLayoutPanel1.BackColor = Color.PeachPuff;
             flowLayoutPanel1.Dock = DockStyle.Bottom;
             flowLayoutPanel1.Location = new Point(0, 603);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
@@ -52,6 +53,7 @@
             // 
             // flowLayoutPanel2
             // 
+            flowLayoutPanel2.BackColor = Color.PeachPuff;
             flowLayoutPanel2.Controls.Add(OpenBtt);
             flowLayoutPanel2.Controls.Add(PrevBtt);
             flowLayoutPanel2.Controls.Add(counter_rotate);
@@ -66,62 +68,74 @@
             // 
             // OpenBtt
             // 
+            OpenBtt.BackColor = Color.Salmon;
+            OpenBtt.Font = new Font("Papyrus", 10.8F, FontStyle.Italic);
             OpenBtt.Location = new Point(3, 3);
             OpenBtt.Name = "OpenBtt";
             OpenBtt.Size = new Size(101, 33);
             OpenBtt.TabIndex = 0;
             OpenBtt.Text = "Open";
-            OpenBtt.UseVisualStyleBackColor = true;
+            OpenBtt.UseVisualStyleBackColor = false;
             OpenBtt.Click += OpenBtt_Click;
             // 
             // PrevBtt
             // 
+            PrevBtt.BackColor = Color.Salmon;
+            PrevBtt.Font = new Font("Papyrus", 10.8F, FontStyle.Italic);
             PrevBtt.Location = new Point(110, 3);
             PrevBtt.Name = "PrevBtt";
             PrevBtt.Size = new Size(108, 34);
             PrevBtt.TabIndex = 2;
             PrevBtt.Text = "Prev";
-            PrevBtt.UseVisualStyleBackColor = true;
+            PrevBtt.UseVisualStyleBackColor = false;
             PrevBtt.Click += PrevBtt_Click;
             // 
             // counter_rotate
             // 
+            counter_rotate.BackColor = Color.Salmon;
+            counter_rotate.Font = new Font("Papyrus", 10.8F, FontStyle.Italic);
             counter_rotate.Location = new Point(224, 3);
             counter_rotate.Name = "counter_rotate";
             counter_rotate.Size = new Size(113, 34);
             counter_rotate.TabIndex = 5;
             counter_rotate.Text = "Rotate";
-            counter_rotate.UseVisualStyleBackColor = true;
+            counter_rotate.UseVisualStyleBackColor = false;
             counter_rotate.Click += button2_Click;
             // 
             // clk_rotate
             // 
+            clk_rotate.BackColor = Color.Salmon;
+            clk_rotate.Font = new Font("Papyrus", 10.8F, FontStyle.Italic);
             clk_rotate.Location = new Point(343, 3);
             clk_rotate.Name = "clk_rotate";
             clk_rotate.Size = new Size(104, 34);
             clk_rotate.TabIndex = 4;
             clk_rotate.Text = "Flip ";
-            clk_rotate.UseVisualStyleBackColor = true;
+            clk_rotate.UseVisualStyleBackColor = false;
             clk_rotate.Click += clk_rotate_Click;
             // 
             // NextBtt
             // 
+            NextBtt.BackColor = Color.Salmon;
+            NextBtt.Font = new Font("Papyrus", 10.8F, FontStyle.Italic);
             NextBtt.Location = new Point(453, 3);
             NextBtt.Name = "NextBtt";
             NextBtt.Size = new Size(104, 34);
             NextBtt.TabIndex = 3;
             NextBtt.Text = "Next";
-            NextBtt.UseVisualStyleBackColor = true;
+            NextBtt.UseVisualStyleBackColor = false;
             NextBtt.Click += NextBtt_Click;
             // 
             // CloseBtt
             // 
+            CloseBtt.BackColor = Color.Salmon;
+            CloseBtt.Font = new Font("Papyrus", 10.8F, FontStyle.Italic);
             CloseBtt.Location = new Point(563, 3);
             CloseBtt.Name = "CloseBtt";
             CloseBtt.Size = new Size(106, 33);
             CloseBtt.TabIndex = 1;
             CloseBtt.Text = "Close";
-            CloseBtt.UseVisualStyleBackColor = true;
+            CloseBtt.UseVisualStyleBackColor = false;
             CloseBtt.Click += CloseBtt_Click;
             // 
             // openFileDialog1
@@ -132,8 +146,9 @@
             // pictureBox1
             // 
             pictureBox1.Anchor = AnchorStyles.None;
-            pictureBox1.BackColor = SystemColors.Window;
+            pictureBox1.BackColor = Color.PowderBlue;
             pictureBox1.ImageLocation = "";
+            pictureBox1.InitialImage = null;
             pictureBox1.Location = new Point(0, 37);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(1082, 566);
